34a43dab7d1f7418d4219bf76a3055066d5b502b
Ralph Tice <ralph.tice@gmail.com>
2015-04-30 14:06:17 -0500

    make start script executable, make unzip idempotent, pull JQ query out

--- consul-vagrant/populate_config.sh
+++ consul-vagrant/populate_config.sh
@@ -4,8 +4,8 @@
 
 curl -X PUT -d 'mysql.internal' http://localhost:8500/v1/kv/web/mysql
 
-# assume there's just one MYSQL host for now
-JQ_QUERY=".[0].Value"
-MYSQL_HOST=`curl http://localhost:8500/v1/kv/web/mysql -s | jq $JQ_QUERY -r | base64 --decode`
+# assume there's just one MYSQL host for now, and use -r to strip enclosing quotes
+JQ_QUERY=".[0].Value -r"
+MYSQL_HOST=`curl http://localhost:8500/v1/kv/web/mysql -s | jq $JQ_QUERY | base64 --decode`
 
 echo $MYSQL_HOST
